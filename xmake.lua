5909bd6d2a0ce1f642426aa33e606d913f3a038e
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-08-01 02:32:01 +0400

    remove: raylib as system dependency, vendor it instead

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,11 @@
 add_rules("mode.debug", "mode.release"); set_defaultmode("debug")
 add_rules("plugin.compile_commands.autoupdate")
 
---- DEPENDENCIES
-add_requires("raylib", {system = true})
-
 --- GLOBAL
 set_languages("c11")
+add_includedirs("src/", "vendor/")
 
 --- TARGETS
 target("ripelime")
-    add_includedirs("src/", "vendor/")
     add_files("src/*.c")
-    add_packages("raylib")
+    add_ldflags("-lraylib")
