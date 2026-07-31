361611b22917976395d7fe5ee149324288d9e83b
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-07-31 23:30:22 +0400

    fix: .gitignore

--- build/.gens/rules/plugin.compile_commands.autoupdate.d
+++
@@ -1,9 +0,0 @@
-{
-    files = {
-        "/home/ziya/dev/main/ripelime/xmake.lua",
-        "/home/ziya/dev/main/ripelime/.xmake/linux/x86_64/xmake.conf"
-    },
-    values = {
-        "src/main.c"
-    }
-}
