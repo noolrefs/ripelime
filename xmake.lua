f9109be28ab5ce11f28124b4fdcf5a2fbdbbf8d2
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-08-01 02:34:53 +0400

    remove: run.sh and bump xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@ add_rules("mode.debug", "mode.release"); set_defaultmode("debug")
 add_rules("plugin.compile_commands.autoupdate")
 
 --- GLOBAL
-set_languages("c11")
+set_languages("c17")
 add_includedirs("src/", "vendor/")
 
 --- TARGETS
